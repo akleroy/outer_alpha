91f1f04493607dd2ef787d73d553db46d99daf68
Karin Sandstrom <sandstrom@mpia.de>
2012-08-14 17:31:55 -0400

    Constants file.

---
+++ code/constants.bat
@@ -0,0 +1,41 @@
+; PHYSICAL CONSTANTS
+
+c = 2.99792458d10       ; speed of light CGS
+h = 6.6260755d-27       ; Planck's constant CGS
+G = 6.67259d-8          ; Grav const CGS
+kb = 1.380658d-16       ; Boltzmann's const CGS
+a = 7.56591d-15         ; Radiation constant CGS
+sb = 5.67051d-5         ; sigma (stefan-boltzmann const) CGS
+qe =  4.803206d-10      ; Charge of electron CGS
+ev =  1.60217733d-12    ; Electron volt CGS
+na =  6.0221367d23      ; Avagadro's Number
+me =  9.1093897d-28     ; electron mass CGS
+mp =  1.6726231d-24     ; proton mass CGS
+mn = 1.674929d-24       ; neutron mass CGS
+mh = 1.673534d-24       ; hydrogen mass CGS
+amu =  1.6605402d-24    ; atomic mass unit CGS
+
+; ASTRONOMICAL CONSTANTS
+
+ms = 1.98900d+33        ; solar mass CGS
+mer = 5.97400d+27       ; earth mass CGS
+mm = 7.35000d+25        ; moon mass CGS
+au = 1.496d13           ; astronomical unit CGS
+pc = 3.0857d18          ; parsec CGS
+yr = 3.155815d7         ; sidereal year CGS
+rs = 6.9599d10          ; sun's radius CGS
+rer = 6.378d8           ; earth's radius CGS
+medd = 3.60271d+34      ; Eddington mass CGS
+jy = 1.d-23             ; Jansky CGS
+sterdeg = 3283          ; Degrees squared in a steradian
+cm2perkkms_hi = 1.823d18  ; Spitzer's HI intensity -> column 
+ksun = 3.28             ; Absolute K magnitude of our sun (Binney & Merrifield)
+restfreq_hi = 1420405751.786 ; HI 21cm transition in Hz
+
+; STRINGS
+
+str_msunpc2 = '!6M!d!9n!6!n pc!u-2!n'
+
+str_msunyr = '!6M!d!9n!6!n yr!u-1!n'
+
+str_msunyrkpc2 = '!6M!d!9n!6!n yr!u-1!n kpc!u-2!n'
